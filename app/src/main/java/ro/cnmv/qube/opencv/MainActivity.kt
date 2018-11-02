1d943b75e3fc8d63d2d4903ea8442a284a2e0ba6
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-11-02 17:52:20 +0200

    Improve performance and detection quality

--- app/src/main/java/ro/cnmv/qube/opencv/MainActivity.kt
+++ app/src/main/java/ro/cnmv/qube/opencv/MainActivity.kt
@@ -6,29 +6,21 @@ import android.content.pm.PackageManager
 import android.graphics.ImageFormat
 import android.hardware.Camera
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
-import android.view.SurfaceHolder
-import android.view.SurfaceView
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
+import android.view.*
 import kotlinx.android.synthetic.main.activity_main.*
-import org.bytedeco.javacpp.Loader
-
 import org.bytedeco.javacv.AndroidFrameConverter
 import org.bytedeco.javacv.OpenCVFrameConverter
-import org.bytedeco.javacpp.Loader.sizeof
-import org.bytedeco.javacpp.helper.opencv_imgproc.cvFindContours
 import org.bytedeco.javacpp.opencv_core.*
 import org.bytedeco.javacpp.opencv_imgproc.*
 
-
 class MainActivity : Activity() {
-    private lateinit var camera: Camera
+    private lateinit var preview: CameraPreview
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
 
         requestPermissions(arrayOf(Manifest.permission.CAMERA), 0)
@@ -37,83 +29,79 @@ class MainActivity : Activity() {
             throw Error("Camera permission not granted")
         }
 
-        val ctx = applicationContext
-
-        val imagesLayout = LinearLayout(ctx)
-        imagesLayout.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-        imagesLayout.orientation = LinearLayout.VERTICAL
-        root.addView(imagesLayout)
-
-        val surfaceView = SurfaceView(ctx)
-        imagesLayout.addView(surfaceView)
-
-        val imageView = ImageView(ctx)
-        imagesLayout.addView(imageView)
-
-        camera = Camera.open()
-        camera.setDisplayOrientation(0)
-
-        val params = camera.parameters
-
-        //val picSizes = params.supportedPreviewSizes
-        //picSizes.forEach { Log.e("PicSize", "${it.width}x${it.height}") }
+        val frameConverter = AndroidFrameConverter()
+        val converter = OpenCVFrameConverter.ToMat()
+        val gray = Mat()
+        val contours = MatVector()
 
         val width = 640
-        val height = 640
+        val height = 480
+
+        preview = CameraPreview(surfaceView, width, height,
+            Camera.PreviewCallback { data, camera ->
+                val frame = frameConverter.convert(data, width, height)
+
+                val image = converter.convertToMat(frame)
+
+                cvtColor(image, gray, COLOR_BGR2GRAY)
+
+                Canny(gray, gray, 100.0, 200.0, 3, false)
+
+                contours.clear()
+                findContours(gray, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE)
+
+                drawContours(image, contours, -1, Scalar(0.0, 255.0, 255.0, 255.0))
+
+                val bitmap = frameConverter.convert(frame)
+                imageView.setImageBitmap(bitmap)
+
+                camera.addCallbackBuffer(data)
+            }
+        )
+    }
+}
+
+class CameraPreview(sv: SurfaceView, w: Int, h: Int, cb: Camera.PreviewCallback) : SurfaceHolder.Callback {
+    private lateinit var camera: Camera
+    private val callback: Camera.PreviewCallback = cb
+
+    init {
+        val holder = sv.holder
+        holder.addCallback(this)
+        holder.setFixedSize(w, h)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        camera = Camera.open()
+        camera.setPreviewDisplay(holder)
+
+        val picSizes = camera.parameters.supportedPreviewSizes
+        picSizes.forEach{ Log.e("Supported size", "${it.width}x${it.height}") }
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        val params = camera.parameters
+
+        val sizes = params.supportedPreviewSizes[0]
 
         with(params) {
             setPreviewSize(width, height)
             previewFormat = ImageFormat.NV21
-
-            focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO
         }
 
         camera.parameters = params
 
-        val surfaceHolder = surfaceView.holder
-        surfaceHolder.setFixedSize(width, height)
+        camera.setPreviewCallbackWithBuffer(callback)
+        val size = params.previewSize
+        val data = ByteArray(size.width * size.height *
+                ImageFormat.getBitsPerPixel(params.previewFormat) / 8)
+        camera.addCallbackBuffer(data)
 
-        surfaceHolder.addCallback(object: SurfaceHolder.Callback {
-            override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
-            }
-
-            override fun surfaceDestroyed(holder: SurfaceHolder) {
-            }
-
-            override fun surfaceCreated(holder: SurfaceHolder) {
-                camera.setPreviewDisplay(holder)
-
-                camera.startPreview()
-
-                val frameConverter = AndroidFrameConverter()
-                val converter = OpenCVFrameConverter.ToMat()
-
-                camera.setPreviewCallback { buffer, _ ->
-                    val frame = frameConverter.convert(buffer, width, height)
-
-                    val image = converter.convertToMat(frame)
-
-                    val gray = Mat()
-                    cvtColor(image, gray, COLOR_BGR2GRAY);
-
-                    val thresh = Mat()
-                    threshold(gray, thresh, 127.0, 255.0, THRESH_BINARY);
-
-                    val contours = MatVector()
-                    findContours(thresh, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE)
-
-                    drawContours(image, contours, -1, Scalar(0.0, 255.0, 255.0, 255.0))
-
-                    val bitmap = frameConverter.convert(frame)
-                    imageView.setImageBitmap(bitmap)
-                }
-            }
-        })
-
+        camera.startPreview()
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        camera.stopPreview()
         camera.release()
     }
 }
